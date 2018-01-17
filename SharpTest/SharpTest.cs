b1cd229dd7ea9a97bebb05184a83decdd843881e
Morasiu <moras2601@wp.pl>
2018-01-17 15:44:02 +0100

    Python testing

--- SharpTest/SharpTest.cs
+++ SharpTest/SharpTest.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Gtk;
 
 namespace SharpTest{
     internal static class SharpTest{
+        private static string osVersion = Environment.OSVersion.ToString();
+        
         public static void Main(string[] args){
             Application.Init();
 
             var window = new Window("Sharp Test");
             window.Resize(600, 400);
             window.SetIconFromFile("/home/hubert/SharpTest/SharpTest/images/icon.png");
-            window.DeleteEvent += new DeleteEventHandler (Window_Delete);
+            window.DeleteEvent += Window_Delete;
 
             var fileButton = new FileChooserButton("Choose file", FileChooserAction.Open);
             
-            var scanButton = new Button("Scan");
-            scanButton.Clicked += new EventHandler(ScanFile);
+            var scanButton = new Button("Scan file");
+            scanButton.SetSizeRequest(100, 50);
+            scanButton.Clicked += (obj, evt) => ScanFile (fileButton.Filename);
+            
+            Console.WriteLine(Environment.OSVersion);
             
             var boxMain = new VBox();
-            boxMain.PackStart(fileButton, false, true, 5);
-            boxMain.PackStart(scanButton, false, false, 5);
+            boxMain.PackStart(fileButton, false, false, 5);
+            boxMain.PackStart(scanButton, false, false, 100);
             window.Add(boxMain);
 
             window.ShowAll();
@@ -31,8 +40,59 @@ namespace SharpTest{
             args.RetVal = true;
         }
 
-        private static void ScanFile(object sender, EventArgs eventArgs){
-            
+        private static void ScanFile(string filename){
+            if (filename.Contains(".py")){
+                Console.WriteLine("Scaning starterd. File: " + filename.Split('/').Last());
+                var rawText = File.ReadAllText(filename);
+                var text = rawText.Split(new[] {"\r\n", "\r", "\n"}, StringSplitOptions.None);
+                var isOOP = IsOOP(text);
+                Console.WriteLine(isOOP ? "Detected objected programing." : "Detected script without OOP.");
+                TestScriptLinux(filename, "Shababa\n");
+                Console.WriteLine(text[1]);
+            }
+            else{
+                Console.WriteLine("Not python :(");
+            }
+        }
+
+        private static bool IsOOP(IEnumerable<string> text){
+            return text.Any(line => line.Contains("class"));
+        }
+
+        private static void TestScriptWindows(string fileName){
+
+            var p = new Process{
+                StartInfo = new ProcessStartInfo(@"C:\Python27\python.exe", fileName){
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            p.Start();
+
+            var output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+        
+        private static void TestScriptLinux(string fileName, string shouldReturn){
+            var p = new Process{
+                StartInfo = new ProcessStartInfo(@"/usr/bin/python", fileName){
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            p.Start();
+
+            var output = p.StandardOutput.ReadToEnd();
+            if (output == shouldReturn){
+                Console.WriteLine("Hurra");
+            }
+            p.WaitForExit();
         }
     }
 }
